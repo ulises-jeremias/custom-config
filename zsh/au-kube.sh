506eee75b950783c1fe07dbf6dd263fa77bed544
ulises-jeremias <ulisescf.24@gmail.com>
2020-09-22 17:39:33 -0300

    Updated dots

---
+++ zsh/au-kube.sh
@@ -0,0 +1,55 @@
+############################################
+################################# KUBERNETES
+
+kubeconfigenv() {
+    export KUBECONFIG="${HOME}/.kube/au-${1}"
+}
+
+kubestaging() {
+    kubeconfigenv staging
+}
+
+kubeprod() {
+    kubeconfigenv prod
+}
+
+kubeclear() {
+    export KUBECONFIG=
+}
+
+kubemanagement() {
+    for pod in $(printf $(kubectl -n management get pods | grep management | awk '{print $1}')); do
+        echo $pod
+        kubectl -n management exec -it "${pod}" -- /bin/bash
+    done
+}
+
+gointomanypodsstaging() {
+    for s in $(kubestaging && kubectl -n $1 get pods | tail -n+2 | awk '{print $1}'); do
+        tab "kubestaging && kubectl -n $1 exec -it $s /bin/bash"
+    done
+}
+
+gointomanypodsprod() {
+    for s in $(kubeprod && kubectl -n $1 get pods | tail -n+2 | awk '{print $1}'); do
+        tab "kubeprod && kubectl -n $1 exec -it $s /bin/bash"
+    done
+}
+
+tab() {
+    local cmd=""
+    local cdto="$PWD"
+    local args="$@"
+
+    if [ -d "$1" ]; then
+        cdto=`cd "$1"; pwd`
+        args="${@:2}"
+    fi
+
+    if [ -n "$args" ]; then
+        cmd="$args"
+    fi
+
+    # TODO: Open diff tabs
+    echo "${cmd}"
+}
