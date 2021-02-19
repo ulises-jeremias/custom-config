abc43838fe98d1bad7e6d04ffbe4c70e1860f477
ulises-jeremias <ulisescf.24@gmail.com>
2021-02-19 10:59:04 -0300

    Updated keybindings

---
+++ zsh/keybindings.sh
@@ -0,0 +1,9 @@
+# bindkey
+bindkey "^U"    backward-kill-line
+bindkey "^u"    backward-kill-line
+bindkey "^[l"   down-case-word
+bindkey "^[L"   down-case-word
+
+# ctrl+<- | ctrl+->
+bindkey "^[[1;5D" backward-word
+bindkey "^[[1;5C" forward-word
